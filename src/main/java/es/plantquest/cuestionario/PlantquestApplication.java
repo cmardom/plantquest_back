ff08b53e7821850218471497b85c4a7657194a18
Carmen <cmardom599@g.educaand.es>
2024-09-20 17:55:36 +0200

    init

---
+++ src/main/java/es/plantquest/cuestionario/PlantquestApplication.java
@@ -0,0 +1,13 @@
+package es.plantquest.cuestionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/plantquest/cuestionario/PlantquestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.plantquest.cuestionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantquestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
