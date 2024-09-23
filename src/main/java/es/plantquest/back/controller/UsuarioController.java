6584438102eaab14a2c393860f295060c5f763e9
Carmen <cmardom599@g.educaand.es>
2024-09-23 11:10:32 +0200

    usuario controller

--- src/main/java/es/plantquest/back/controller/UsuarioController.java
+++ src/main/java/es/plantquest/back/controller/UsuarioController.java
@@ -4,10 +4,8 @@ import es.plantquest.back.domain.Usuario;
 import es.plantquest.back.service.UsuarioService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,29 @@ public class UsuarioController {
     @GetMapping({"","/"})
     public List<Usuario> getUsuarios() {
         log.info("getUsuarios");
-        return this.usuarioService.g
-    }>
+        return this.usuarioService.all();
+    }
+
+    @PostMapping({"","/"})
+    public Usuario nuevoUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario one(@PathVariable("id") Long id) {
+        return usuarioService.one(id);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario replaceUsuario(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+        return usuarioService.replace(id, usuario);
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable("id") Long id) {
+        usuarioService.delete(id);
+    }
+
 }
