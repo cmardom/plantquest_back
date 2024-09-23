a8937a4ba25f6f36200ce543482ef8d2bd554ee2
Carmen <cmardom599@g.educaand.es>
2024-09-23 14:24:09 +0200

    landing init

--- src/main/angular/app-front/src/app/app.component.ts
+++ src/main/angular/app-front/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {FormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
 import {HeaderComponent} from "./header/header.component";
+import {LandingComponent} from "./landing/landing.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, LoginComponent, HeaderComponent],
+  imports: [RouterOutlet, FormsModule, LoginComponent, HeaderComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
