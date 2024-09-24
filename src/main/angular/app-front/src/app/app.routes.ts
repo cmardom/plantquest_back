22c3b4761a46f1c15f7946050bc56280ca77b1f3
Carmen <cmardom599@g.educaand.es>
2024-09-24 11:39:49 +0200

    funciona el routing

--- src/main/angular/app-front/src/app/app.routes.ts
+++ src/main/angular/app-front/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {GuiaComponent} from "./guia/guia.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home', component: LandingComponent},
+  {path:'guia', component: GuiaComponent},
+
+  //{path:'campings/nuevo', component: CampingsFormularioComponent},
+  //{path:'campings/:id', component: CampingsFormularioComponent},
+  {path:'**', pathMatch:'full', redirectTo: 'home'}
+];
