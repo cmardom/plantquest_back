5979011cfdde72c12822b8a818629d5d338e6b7d
Carmen <cmardom599@g.educaand.es>
2024-09-24 12:00:09 +0200

    interfaz

---
+++ src/main/angular/app-front/src/app/interfaces/planta-interface.ts
@@ -0,0 +1,15 @@
+// planta.model.ts
+export interface Planta {
+  id: number;
+  nombre_cientifico: string;
+  nombre_comun: string;
+  riego: string;
+  iluminacion: string;
+  humedad: string;
+  temperatura: string;
+  localizacion: string;
+  toxicidad: string;
+  abono: string;
+  caracteristicas: string;
+  imagePath: string;
+}
