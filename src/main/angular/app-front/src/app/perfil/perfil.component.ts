9fb79db3b09f63bc7402e027e67f7e294da47cfc
Carmen <cmardom599@g.educaand.es>
2024-12-01 12:13:23 +0100

    removed un-needed code and cleanUp

--- src/main/angular/app-front/src/app/perfil/perfil.component.ts
+++ src/main/angular/app-front/src/app/perfil/perfil.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import { Usuario } from "../interfaces/usuario";
-import { NgForOf, NgIf } from "@angular/common";
+import {DOCUMENT, isPlatformBrowser, NgForOf, NgIf} from "@angular/common";
 import { UsuarioService } from "../services/usuario.service";
 import { ColeccionService } from "../services/coleccion.service";
 import {PlantaService} from "../services/planta.service";
@@ -21,10 +21,25 @@ export class PerfilComponent /*implements OnInit*/ {
   constructor(public usuarioService: UsuarioService,
               private coleccionService: ColeccionService,
               private plantaService: PlantaService,
-              private router: Router) { }
+              private router: Router,
+              @Inject(PLATFORM_ID) private platformId: object,
+              @Inject(DOCUMENT) private document: Document
+  ) { }
+
 
   async cerrarSession(){
     await this.usuarioService.logout();
     await this.router.navigate(['/home']);
+  }
+
+  toggleEdit = () => {
+    if(isPlatformBrowser(this.platformId) && this.document){
+      // this.document.querySelector('.usuario-form').classList.toggle('editando');
+      const usuarioForm = this.document.querySelector('.usuario-form');
+      if(usuarioForm && usuarioForm instanceof HTMLElement){
+        usuarioForm.classList.toggle('editando');
+      }
+    }
+
   }
 }
